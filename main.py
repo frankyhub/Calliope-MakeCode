a85a79b2f241afb99e9c113dd3fdabef65e5ba32
frankyhub <61152841+frankyhub@users.noreply.github.com>
2020-09-23 06:02:34 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.py
@@ -0,0 +1,43 @@
+Zufall = 0
+
+def on_forever():
+    global Zufall
+    if input.button_is_pressed(Button.A):
+        basic.clear_screen()
+        Zufall = randint(1, 4)
+    elif False:
+        pass
+    else:
+        if Zufall == 1:
+            basic.show_leds("""
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                """)
+        elif Zufall == 2:
+            basic.show_leds("""
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                """)
+        elif Zufall == 3:
+            basic.show_leds("""
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                """)
+        elif Zufall == 4:
+            basic.show_leds("""
+                # . . . #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+                """)
+basic.forever(on_forever)
