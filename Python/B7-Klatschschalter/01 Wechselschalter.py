55ab1a5098566e56fc9ff4cc393904773be376a3
frankyhub <61152841+frankyhub@users.noreply.github.com>
2020-09-25 19:22:16 +0200

    Add files via upload

---
+++ Python/B7-Klatschschalter/01 Wechselschalter.py
@@ -0,0 +1,15 @@
+LED = 0
+
+def on_forever():
+    global LED
+    if input.button_is_pressed(Button.A):
+        if LED == 1:
+            LED = 0
+            if LED == 0:
+                basic.turn_rgb_led_off()
+        elif LED == 0:
+            LED = 1
+            if LED == 1:
+                basic.set_led_color(0xff0000)
+        basic.pause(500)
+basic.forever(on_forever)
