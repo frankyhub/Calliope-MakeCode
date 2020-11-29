ef6d27ae9ae2526794bb379354787f47be490c38
frankyhub <61152841+frankyhub@users.noreply.github.com>
2020-11-29 19:54:47 +0100

    Delete 12 Treppenhaus.py

--- WS V2 Programme/12 Treppenhaus.py
+++
@@ -1,6 +0,0 @@
-def on_forever():
-    basic.turn_rgb_led_off()
-    if input.button_is_pressed(Button.A):
-        basic.set_led_color(0xff0000)
-        basic.pause(2000)
-basic.forever(on_forever)
